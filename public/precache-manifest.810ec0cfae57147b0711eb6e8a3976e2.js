b6b8ee14736071e205a0bddaae2c16de0e92f6fe
edcwsxqaz12337@gmail.com <edcwsxqaz12337@gmail.com>
2019-09-15 12:54:36 +0800

    first commit

---
+++ public/precache-manifest.810ec0cfae57147b0711eb6e8a3976e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5d37ac41408f89156bd84babb9e986b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f14372ea1b73982348a9",
+    "url": "/static/css/main.491eafad.chunk.css"
+  },
+  {
+    "revision": "9a27411965263a825c50",
+    "url": "/static/js/2.54368cd6.chunk.js"
+  },
+  {
+    "revision": "f14372ea1b73982348a9",
+    "url": "/static/js/main.d83e0f42.chunk.js"
+  },
+  {
+    "revision": "ff8fc16a939599321a27",
+    "url": "/static/js/runtime~main.73aae552.js"
+  }
+]);
